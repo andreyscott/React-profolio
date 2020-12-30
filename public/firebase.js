aa9460ce7013034069c47236e2fe13dc07570f76
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-12-30 23:44:30 +0100

    the second commit of the day

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDI5zCSNO3JUF1ZIIcyPao0yb64QgYGjWc",
+    authDomain: "react-portfolio-cab52.firebaseapp.com",
+    projectId: "react-portfolio-cab52",
+    storageBucket: "react-portfolio-cab52.appspot.com",
+    messagingSenderId: "231086733295",
+    appId: "1:231086733295:web:0aacedbb50b4abb2c4a81a",
+    measurementId: "G-WB412XHR8E"
+  };
